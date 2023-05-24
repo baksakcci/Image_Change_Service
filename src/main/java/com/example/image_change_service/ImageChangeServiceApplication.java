d4fa29219cd795cdb912211aa014cf8e99ccf0da
baksakcci <pshsh910@gmail.com>
2023-05-24 20:57:58 +0900

    refactor: 개발, 배포 환경 분리
    - dev, prod 2개 환경으로 분리하여 배포 진행
    - main 코드에 설정 추가했던 기존 방식에서 spring.profiles.active="" 방식을 활용하여 환경설정 분리

--- src/main/java/com/example/image_change_service/ImageChangeServiceApplication.java
+++ src/main/java/com/example/image_change_service/ImageChangeServiceApplication.java
@@ -1,27 +1,12 @@
 package com.example.image_change_service;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ImageChangeServiceApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
-
-    /*static {
-        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-    }
-    */
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(ImageChangeServiceApplication.class)
-                .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }
-
 }
